c983c297a64f0ee73015c8613203a3b7169074ba
MahmoudMostafa91 <mahmoudmostafa91@gmail.com>
2019-06-15 16:28:39 +0200

    add header component and jquery,bootstrap

--- Dr.Care/src/app/app.module.ts
+++ Dr.Care/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
+import { UpperHeaderComponent } from './core/header/upper-header/upper-header.component';
+import { LowerHeaderComponent } from './core/header/lower-header/lower-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UpperHeaderComponent,
+    LowerHeaderComponent
   ],
   imports: [
     BrowserModule,
