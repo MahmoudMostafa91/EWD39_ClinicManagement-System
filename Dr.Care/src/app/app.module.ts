add981f8f560b768d932b3440964a6176aee0e43
MahmoudMostafa91 <mahmoudmostafa91@gmail.com>
2019-06-18 21:01:29 +0200

    add header and footer sepearated

--- Dr.Care/src/app/app.module.ts
+++ Dr.Care/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { UpperHeaderComponent } from './core/header/upper-header/upper-header.component';
 import { LowerHeaderComponent } from './core/header/lower-header/lower-header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UpperHeaderComponent,
-    LowerHeaderComponent
+    LowerHeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
